abc5f7dbfe9211402a762acc00c54cf02797c490
Pooja Shelar <pooja.shelar43@gmail.com>
2023-06-20 15:07:27 +0530

    - Fixed docker container exit issue - Added few more details in readme.md

--- Dockerfile
+++ Dockerfile
@@ -1,9 +1,9 @@
 FROM python:3
-WORKDIR /app
-COPY requirements.txt ./
+WORKDIR /app/src
+COPY requirements.txt .
 RUN pip install -r requirements.txt
-COPY src src
+COPY src .
 EXPOSE 5000
 HEALTHCHECK --interval=30s --timeout=30s --start-period=5s --retries=3 \
         CMD curl -f http://localhost:5000/health || exit 1
-ENTRYPOINT [ "python", "./src/app.py" ]
+ENTRYPOINT [ "python3", "app.py" ]
--- src/app.py
+++ src/app.py
@@ -3,45 +3,42 @@ import os
 from dbio.dbutils import DbUtils
 from admin import McqExamCenterServices
 
-def create_app():
-    ''' Description: Create and return Flask app object.
-        Provide sqlite db configuration to start db when app is up.
-        SECRET_KEY is for data safety and used by Flask
-    '''
-    app = Flask(__name__, instance_relative_config=True)
+''' Description: Create and return Flask app object.
+    Provide sqlite db configuration to start db when app is up.
+    SECRET_KEY is for data safety and used by Flask
+'''
+app = Flask(__name__, instance_relative_config=True)
 
-    #Define config mapping- where to look for configuration file
-    app.config.from_mapping(
-        SECRET_KEY='dev',
-        DATABASE= os.path.join(app.instance_path, 'MCQCenter.sqlite'),
-        TEMPFILE= app.instance_path,
+#Define config mapping- where to look for configuration file
+app.config.from_mapping(
+    SECRET_KEY='dev',
+    DATABASE= os.path.join(app.instance_path, 'MCQCenter.sqlite'),
+    TEMPFILE= app.instance_path,
+)
+
+#load configurations from file
+app.config.from_pyfile('config.py', silent=True)
+# ensure the instance folder exists
+try:
+    os.makedirs(app.instance_path)
+except OSError:
+    pass
+
+@app.route("/health")
+def health():
+    return jsonify(
+        status="Up"
     )
 
-    #load configurations from file
-    app.config.from_pyfile('config.py', silent=True)
-    # ensure the instance folder exists
-    try:
-        os.makedirs(app.instance_path)
-    except OSError:
-        pass
-
-    @app.route("/health")
-    def health():
-        return jsonify(
-            srarus="Up"
-        )
-
-    @app.route("/")
-    def welcomePage():
-        return render_template("index.html")
+@app.route("/")
+def welcomepage():
+    return render_template("index.html")
     
-    #Register db with app and init
-    DbUtils.init_app(app)
+#Register db with app and init
+DbUtils.init_app(app)
 
-    #Register blueprint with app
-    app.register_blueprint(McqExamCenterServices.bp)
+#Register blueprint with app
+app.register_blueprint(McqExamCenterServices.bp)
 
-    if __name__ == '__main__':
-        app.run(host='0.0.0.0', port=5000)
-
-    return app
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
